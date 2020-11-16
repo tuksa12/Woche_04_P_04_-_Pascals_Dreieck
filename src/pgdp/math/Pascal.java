1b1cff0af7f90790c6a5a8152962f565b18073d1
Martin Dunker <ga27kin@mytum.de>
2020-11-16 23:23:21 +0100

    Exercise-Template pushed by Artemis

---
+++ src/pgdp/math/Pascal.java
@@ -0,0 +1,14 @@
+package pgdp.math;
+
+import static pgdp.MiniJava.*;
+
+class Pascal {
+    public static int[][] pascalTriangle(int n) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // TODO
+    }
+}
