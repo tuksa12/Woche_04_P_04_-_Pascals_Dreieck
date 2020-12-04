8c1672e357a59b9a463109243c998c269dea4145
DESKTOP-2KIND8H\Raphael Tuksa <rapha.tuksa@hotmail.com>
2020-12-04 12:18:07 +0100

    12

--- src/pgdp/math/Pascal.java
+++ src/pgdp/math/Pascal.java
@@ -4,11 +4,27 @@ import static pgdp.MiniJava.*;
 
 class Pascal {
     public static int[][] pascalTriangle(int n) {
-        // TODO
-        return null;
+        int[][] triangle = new int[n][];
+        for (int i = 0; i < n; i++) {
+            triangle[i] = new int[i + 1];
+            triangle[i][0] = 1;
+            triangle[i][i] = 1;
+            for (int j = 1; j < i; j++)
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+        return triangle;
     }
 
     public static void main(String[] args) {
-        // TODO
+        int numberOfLines = read("Gib die Zeilenzahl an:");
+        int[][] triangle = pascalTriangle(numberOfLines);
+        for (int i = 0; i < numberOfLines; i++) {
+            writeConsole("n=" + i);
+            for (int j = 0; j < triangle[i].length; j++) {
+                writeConsole("\t");
+                writeConsole(triangle[i][j]);
+            }
+            writeLineConsole();
+        }
     }
 }
